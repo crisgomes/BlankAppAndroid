1a49e641cd1c01c3126f89814967040df9048359
Cristiano Gomes <crgomes@microsoft.com>
2017-11-06 00:34:48 -0200

    inicial

--- BlankAppAndroid/MainActivity.cs
+++ BlankAppAndroid/MainActivity.cs
@@ -1,6 +1,9 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
 
 namespace BlankAppAndroid
 {
@@ -10,6 +13,9 @@ namespace BlankAppAndroid
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+
+            MobileCenter.Start("054072dd-117a-45b4-85cc-7a3595c1d8ac",
+                   typeof(Analytics), typeof(Crashes));
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
